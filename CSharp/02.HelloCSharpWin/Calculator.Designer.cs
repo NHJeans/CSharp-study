3e01e3b28b9ba5a046d3dd0575ae0a53c49b1a87
NHJeans <guswo1013@naver.com>
2025-01-07 15:26:11 +0900

    세번째 강의 업로드

--- CSharp/02.HelloCSharpWin/Calculator.Designer.cs
+++ CSharp/02.HelloCSharpWin/Calculator.Designer.cs
@@ -30,6 +30,10 @@ namespace _02.HelloCSharpWin
         private void InitializeComponent()
         {
             this.HelloLabel = new System.Windows.Forms.Label();
+            this.Sum1 = new System.Windows.Forms.TextBox();
+            this.Sum2 = new System.Windows.Forms.TextBox();
+            this.SumNumbers = new System.Windows.Forms.Button();
+            this.SumResult = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // HelloLabel
@@ -43,11 +47,47 @@ namespace _02.HelloCSharpWin
             this.HelloLabel.Text = "여기를 클릭하세요";
             this.HelloLabel.Click += new System.EventHandler(this.HelloLabel_Click);
             // 
+            // Sum1
+            // 
+            this.Sum1.Location = new System.Drawing.Point(22, 94);
+            this.Sum1.Name = "Sum1";
+            this.Sum1.Size = new System.Drawing.Size(100, 23);
+            this.Sum1.TabIndex = 1;
+
+            // 
+            // Sum2
+            // 
+            this.Sum2.Location = new System.Drawing.Point(147, 95);
+            this.Sum2.Name = "Sum2";
+            this.Sum2.Size = new System.Drawing.Size(100, 23);
+            this.Sum2.TabIndex = 2;
+            // 
+            // SumNumbers
+            // 
+            this.SumNumbers.Location = new System.Drawing.Point(274, 94);
+            this.SumNumbers.Name = "SumNumbers";
+            this.SumNumbers.Size = new System.Drawing.Size(75, 23);
+            this.SumNumbers.TabIndex = 3;
+            this.SumNumbers.Text = "=";
+            this.SumNumbers.UseVisualStyleBackColor = true;
+            this.SumNumbers.Click += new System.EventHandler(this.SumNumbers_Click);
+            // 
+            // SumResult
+            // 
+            this.SumResult.Location = new System.Drawing.Point(376, 94);
+            this.SumResult.Name = "SumResult";
+            this.SumResult.Size = new System.Drawing.Size(100, 23);
+            this.SumResult.TabIndex = 4;
+            // 
             // Calculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(505, 482);
+            this.Controls.Add(this.SumResult);
+            this.Controls.Add(this.SumNumbers);
+            this.Controls.Add(this.Sum2);
+            this.Controls.Add(this.Sum1);
             this.Controls.Add(this.HelloLabel);
             this.Name = "Calculator";
             this.Text = "Form1";
@@ -59,6 +99,10 @@ namespace _02.HelloCSharpWin
         #endregion
 
         private System.Windows.Forms.Label HelloLabel;
+        private System.Windows.Forms.TextBox Sum1;
+        private System.Windows.Forms.TextBox Sum2;
+        private System.Windows.Forms.Button SumNumbers;
+        private System.Windows.Forms.TextBox SumResult;
     }
 }
 
